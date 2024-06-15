b53426ca1b5e670c71e91015e540e4e852c0786e
Krizia Arias <ariaskrizia@gmail.com>
2024-06-14 23:26:20 -0400

    se modifica js

--- js/main.js
+++ js/main.js
@@ -1,2 +1,9 @@
-
+const email = document.getElementById("correo");
+email.addEventListener("correo", function (event) {
+    if (email.validity.typeMismatch) {
+        email.setCustomValidity("¡Se esperaba una dirección de correo electrónico!");
+    } else {
+    email.setCustomValidity("correo valido!");
+}
+});
 
